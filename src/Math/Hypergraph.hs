b09c3f20b932e98dd843581deecf3476b7f53676
ggsab <guillaume.sabbagh@etu.utc.fr>
2024-11-27 18:15:58 +0100

    Hypergraph definition

--- src/Math/Hypergraph.hs
+++ src/Math/Hypergraph.hs
@@ -1,3 +1,9 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MonadComprehensions #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 {-| Module  : HypergraphRewriting
 Description : A hypergraph is a finite labeled directed hypergraphs in this library.
 Copyright   : Juan Meleiro & Guillaume Sabbagh
@@ -12,19 +18,27 @@ A hypergraph is a finite labeled directed hypergraphs in this library.
 
 module Math.Hypergraph
 (
-
+    -- * Hypergraph
+    Hyperedge(..),
+    hyperedge,
+    arity,
+    coarity,
+    Hypergraph,
+    -- ** Getters
+    vertices,
+    hyperedges,
+    -- ** Smart constructors
+    HypergraphError(..),
+    hypergraph,
+    unsafeHypergraph,
 )
 
 where
     import              Data.WeakSet         (Set)
     import qualified    Data.WeakSet       as Set
     import              Data.WeakSet.Safe
-    import              Data.WeakMap         (Map)
-    import qualified    Data.WeakMap       as Map
-    import              Data.WeakMap.Safe
     import              Data.Simplifiable
-    import              Data.Maybe           (isJust, isNothing, fromJust)
-    import              Data.Either          (isLeft)
+    import              Math.IO.PrettyPrint
 
     import              Math.FiniteCategory
    
@@ -32,3 +46,68 @@ where
     import              GHC.Generics
 
     
+    
+    
+    -- | An 'Hyperedge' is composed of a label in a signature 's', a list of source vertices, a list of target vertices and a hyperedge identifier.
+    data Hyperedge n e s = Hyperedge{
+                            idHyperedge :: e,
+                            sourceHyperedge :: [n],
+                            targetHyperedge :: [n],
+                            labelHyperedge :: s
+                            }
+                          deriving (Eq, Show, Generic, Simplifiable)
+                          
+    -- | Constructor for 'Hyperedge'.
+    hyperedge :: e -> [n] -> [n] -> s -> Hyperedge n e s
+    hyperedge e ss ts l = Hyperedge{
+                            idHyperedge = e,
+                            sourceHyperedge = ss,
+                            targetHyperedge = ts,
+                            labelHyperedge = l
+                            }
+                          
+    -- | The arity of a hyperedge is its number of sources.
+    arity :: Hyperedge n e s -> Int
+    arity e = length $ sourceHyperedge e
+    
+    -- | The coarity of a hyperedge is its number of targets.
+    coarity :: Hyperedge n e s -> Int
+    coarity e = length $ targetHyperedge e
+    
+    
+    instance (PrettyPrint n, PrettyPrint e, PrettyPrint s) => PrettyPrint (Hyperedge n e s) where
+        pprint v a = (pprint v $ idHyperedge a)++ " : " ++(pprint v $ sourceHyperedge a)++"-"++(pprint v $ labelHyperedge a)++"->"++(pprint v $ targetHyperedge a)
+        
+
+    -- | A 'Hypergraph' is a set of vertices and a set of 'Hyperedge's.
+    -- 
+    -- 'Hypergraph' is private, use smart constructor 'hypergraph'.
+    data Hypergraph n e s = Hypergraph {
+                        vertices :: Set n, -- ^ The set of vertices of the hypergraph.
+                        hyperedges :: Set (Hyperedge n e s) -- ^ The set of hyperedges of the hypergraph.
+                        } deriving (Eq, Generic, PrettyPrint, Simplifiable)
+    
+    instance (Show n, Show e, Show s) => Show (Hypergraph n e s) where
+        show g = "(unsafeHypergraph "++(show $ vertices g)++" "++(show $ hyperedges g)++")"
+    
+    
+    -- | An error when constructing a hypergraph.
+    data HypergraphError n e s = UnknownVertex n
+                               | IncompatibleArity (Hyperedge n e s) (Hyperedge n e s)
+                               deriving (Eq, Show, Generic, PrettyPrint, Simplifiable)
+    
+    -- | Smart constructor of 'Hypergraph'.
+    hypergraph :: (Eq n, Eq s) => Set n -> Set (Hyperedge n e s) -> Either (HypergraphError n e s) (Hypergraph n e s)
+    hypergraph ns es
+        | not $ Set.null $ unknownSources = Left $ UnknownVertex $ anElement unknownSources
+        | not $ Set.null $ unknownTargets = Left $ UnknownVertex $ anElement unknownTargets
+        | not $ Set.null $ incompatibleHyperedges = Left $ uncurry IncompatibleArity $ anElement incompatibleHyperedges
+        | otherwise = Right Hypergraph{vertices=ns, hyperedges=es}
+        where
+            unknownSources = (set $ Set.concat $ sourceHyperedge <$> es) |-| ns
+            unknownTargets = (set $ Set.concat $ targetHyperedge <$> es) |-| ns
+            incompatibleHyperedges = [(e1,e2) | e1 <- es, e2 <- es, labelHyperedge e1 == labelHyperedge e2 && (arity e1 /= arity e2 || coarity e1 /= coarity e2)]
+        
+    -- | Unsafe constructor of 'Hypergraph', does not check the 'Hypergraph' structure.
+    unsafeHypergraph :: Set n -> Set (Hyperedge n e s) -> Hypergraph n e s
+    unsafeHypergraph n e = Hypergraph{vertices=n, hyperedges=e}
--- test/Main.hs
+++ test/Main.hs
@@ -1,6 +1,18 @@
 module Main (main) where
 
-import Math.Hypergraph
+import              Data.WeakSet         (Set)
+import qualified    Data.WeakSet       as Set
+import              Data.WeakSet.Safe
+import              Math.Hypergraph
+
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = do
+    let nodes = set $ [0,1,2]
+    let edge1 = hyperedge 0 [0,1] [1,2] "f"
+    let edge2 = hyperedge 0 [0,1] [1] "f"
+    let edge3 = hyperedge 0 [0,1] [1] "g"
+    let h1 = hypergraph nodes (set [edge1,edge2])
+    let h2 = hypergraph nodes (set [edge1,edge3])
+    putStrLn $ show $ h1
+    putStrLn $ show $ h2
